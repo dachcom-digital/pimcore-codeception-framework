434621f8947c950ed1fd0842cf9bac28e367c486
Stefan Hagspiel <shagspiel@dachcom.ch>
2021-09-20 16:32:36 +0200

    add dev-requirement to composer file

--- src/_etc/scripts/composer.sh
+++ src/_etc/scripts/composer.sh
@@ -6,17 +6,22 @@ source $PIMCORE_CODECEPTION_FRAMEWORK/src/_etc/scripts/yaml_reader.sh
 
 eval "$(parse_yaml $TEST_BUNDLE_TEST_DIR/_etc/config.yml)"
 
-## define structure (bundle is available under lib/test-bundle)
-## add test-bundle composer.json to skeleton composer.json
-composer config repositories.local '{"type": "path", "url": "./lib/test-bundle", "options": {"symlink": true}}' --file composer.json
-
-# add TestKernel to autoload-dev
-sed -i '$s%}%,"autoload-dev":{"classmap" : ["src/TestKernel.php"]} }%' composer.json
-
 # define variables
 PACKAGES=''
 NODE='additional_composer_packages'
 PACKAGE_GITHUB_REPOSITORY=$(echo $GITHUB_REPOSITORY |sed  s/pimcore-//)
+REQUIRE_DEV_DATA=$(sed -nr '/.*(\brequire-dev\b).*\{/,/\}/p' ./lib/test-bundle/composer.json)
+SAVE_REQUIRE_DEV_DATA=$(printf "%s\n" "$REQUIRE_DEV_DATA" | sed -e 's/[]\/${}\n"*.:^[]/\\&/g' | tr -d '\n' | sed -z 's/\(.*\}\),/\1/');
+
+## define structure (bundle is available under lib/test-bundle)
+## add test-bundle composer.json to skeleton composer.json
+composer config repositories.local '{"type": "path", "url": "./lib/test-bundle", "options": {"symlink": true}}' --file composer.json
+
+# replace require-dev from bundle
+sed -i "/require-dev/{:a;N;/}/!ba;N;s/.*\n/$SAVE_REQUIRE_DEV_DATA,\n/}" composer.json
+
+# add TestKernel to autoload-dev
+sed -i '$s%}%,"autoload-dev":{"classmap" : ["src/TestKernel.php"]} }%' composer.json
 
 for CURRENT_CONFIG_NODE in ${__}; do
   if [ $CURRENT_CONFIG_NODE != $NODE ]; then continue; fi
